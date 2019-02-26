cab2528d7ffc3c21e68fe75387f8395a66a3fdb6
Rodrigo Valle <rdan.valle@gmail.com>
2019-02-26 00:45:11 -0800

    Cleanup + documentation of KalmanFilter::new

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,30 @@ extern crate ndarray;
 use ndarray::{ArrayViewMut1, Array1, Array2, Axis};
 
 struct KalmanFilter {
-    state: Array1<f64>,  // N vector representing the system's current state
-    covariance: Array2<f64>,  // NxN matrix
-    F_k: Array2<f64>,
+    state_vec: Array1<f64>,  // N vector representing the system's current state
+    cov_mat: Array2<f64>,    // NxN covariance matrix
+    pred_mat: Array2<f64>,   // prediction matrix (F_k in academic literature)
 }
 
 impl KalmanFilter {
-    // predict is a function that mutates a state vector into the next state
-    // vector
+    // state_vec: state that the system begins in
+    // predict_fn: mutates a state vec into a prediction of the next state vec
     fn new(
-        state: Array1<f64>,
-        predict: fn(ArrayViewMut1<f64>),
+        state_vec: Array1<f64>,
+        predict_fn: fn(ArrayViewMut1<f64>),
     ) -> KalmanFilter {
-        let n = state.dim();
+        let n = state_vec.dim();
 
-        // iterate through all unit vectors and call predict to construct the
-        // matrix F_k, the prediction matrix
-        let mut F_k = Array2::<f64>::eye(n);
-        F_k.axis_iter_mut(Axis(1)).for_each(predict);
+        // iterate through all unit vectors (columns of the identity matrix) and
+        // call predict_fn to construct the prediction matrix, a matrix which
+        // transforms any state vec into the prediction of the next state vec
+        let mut pred_mat = Array2::<f64>::eye(n);
+        pred_mat.axis_iter_mut(Axis(1)).for_each(predict_fn);
 
         KalmanFilter {
-            state,
-            F_k,
-            covariance: Array2::ones((n, n)),
+            state_vec,
+            pred_mat,
+            cov_mat: Array2::ones((n, n)),
         }
     }
 }
@@ -45,12 +46,15 @@ mod tests {
         state_vec[1] = v;
     }
 
-    // test predict with a 
-    #[test] fn test_predict() {
+    // Test KalmanFilter::new to make sure it creates the appropriate prediction
+    // matrix. For this test, the state vector is (x, v)^T, where x is an
+    // x-coordinate and v is velocity, and the prediction function predicts the
+    // next x-coordinate after a timestep assuming velocity doesn't change.
+    #[test] fn test_predict_matrix() {
         let state = arr1(&[0.0, 10.0]);
         let kf = KalmanFilter::new(state, predict);
 
-        assert!(kf.F_k == arr2(&[[1.0, TIMESTEP],
-                                 [0.0, 1.0     ]]))
+        assert!(kf.pred_mat == arr2(&[[1.0, TIMESTEP],
+                                      [0.0, 1.0     ]]))
     }
 }
